a15e3cb10fb790cc63655d0676bcadf84dfc492e
dikhacoundoul-sys <dikhacoundoul@gmail.com>
2026-09-04 12:40:21 +0000

    Mise a jour de la configuration de la base de donnees

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 4000,
+        'port'         => 3306,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
